42550db469bbd98d41f31535b57790709cdfc736
Ypriom121 <pritomgupta121@gmail.com>
2023-11-25 11:16:55 +0600

    find view profile to single data have finished

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0kobzro.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,13 +24,34 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+//     await client.connect();
+    const matrimonyCollection = client.db("matrimonyDb").collection("blog");
+
+  app.get('/blog',async(req,res)=>{
+       const result = await matrimonyCollection.find().toArray()
+       res.send(result)
+  })
+//  find the view profile
+  app.get('/blog/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id:new ObjectId(id)}
+    const result = await matrimonyCollection.findOne(query)
+    res.send(result)
+  })
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+//     await client.close();
   }
 }
 run().catch(console.dir);
